cad41e78fe51e4413a48b067044fe427a99f7838
Oscar Arbelaez <oscar@arbelaez.dev>
2022-03-15 09:10:48 +0000

    Ponder it

---
+++ src/deepthought/ponder.py
@@ -0,0 +1,3 @@
+def compute_ultimate_answer():
+    # TODO: Actually ponder for seven and a half million years
+    return 42
---
+++ tests/test_ponder.py
@@ -0,0 +1,5 @@
+from deepthought.ponder import compute_ultimate_answer
+
+
+def test_ultimate_answer():
+    assert compute_ultimate_answer() == 42
